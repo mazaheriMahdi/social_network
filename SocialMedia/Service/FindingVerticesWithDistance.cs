7bdf52d1f3dd245a0d21c9d456b3fb6d49509bc2
Mobin <mobin.aranii@gmail.com>
2024-02-02 02:02:23 +0330

    add findingDistanceVertices service

---
+++ SocialMedia/Service/FindingVerticesWithDistance.cs
@@ -0,0 +1,34 @@
+﻿using SocialMedia.Graph;
+
+namespace SocialMedia.Service
+{
+    public class FindingVerticesWithDistance<E, V> : IFindingVerticesWithDistance<E, V>
+    {
+        public List<Vertex<E, V>> findVertices(AdjacencyMapGraph<E, V> graph,Vertex<E, V> v, int distance)
+        {
+            var known = new List<Vertex<E, V>>();
+            var level = new List<Vertex<E, V>>();
+            known.Add(v);
+            level.Add(v);
+            while (distance > 0)
+            {
+                var nexLevel = new List<Vertex<E, V>>();
+                foreach(var ver in level)
+                {
+                    foreach(var edge in graph.outgoingEdges(v))
+                    {
+                        var tempVertex = graph.opposite(ver, edge);
+                        if (!known.Contains(tempVertex))
+                        {
+                            known.Add(tempVertex);
+                            nexLevel.Add(tempVertex);
+                        }
+                    }
+                }
+                level = nexLevel;
+                distance--;
+            }
+            return level;
+        }
+    }
+}
