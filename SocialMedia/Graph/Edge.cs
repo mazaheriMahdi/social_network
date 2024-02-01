9d508dddc76921579d91bbeeeb982004c414a174
Mobin <mobin.aranii@gmail.com>
2024-02-01 20:08:05 +0330

    add Edge

---
+++ SocialMedia/Graph/Edge.cs
@@ -0,0 +1,18 @@
+﻿namespace SocialMedia.Graph
+{
+    public class Edge<E, V>
+    {
+        private E element;
+        private List<Edge<E, V>> pos;
+        private Vertex<E, V>[] endpoints;
+        public Edge(Vertex<E, V> u, Vertex<E, V> v, E element)
+        {
+            this.element = element;
+            endpoints = (Vertex<E, V>[])new Vertex<E, V>[] { u, v };
+        }
+        public E getElement() { return this.element; }
+        public Vertex<E, V>[] getEndpoints() { return this.endpoints; }
+        public void setPositions(List<Edge<E, V>> p) { this.pos = p; }
+        public List<Edge<E, V>> getPosition() { return this.pos; }
+    }
+}
