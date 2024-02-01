cb387bc2a2147f1102e62a4d370bfce0abb97e3f
Mahdi mazaheri <mahdi.mazaheri8383@gmail.com>
2024-02-01 23:48:35 +0330

    add ScoringAlgorithm.cs

---
+++ SocialMedia/Service/IScoringAlgorithm.cs
@@ -0,0 +1,8 @@
+using SocialMedia.Models.BusinessModels;
+
+namespace SocialMedia.Service;
+
+public interface IScoringAlgorithm
+{
+    public int Score(User a, User b);
+}
---
+++ SocialMedia/Service/ScoringAlgorithm.cs
@@ -0,0 +1,11 @@
+using SocialMedia.Models.BusinessModels;
+
+namespace SocialMedia.Service;
+
+public class ScoringAlgorithm : IScoringAlgorithm
+{
+    public int Score(User a, User b)
+    {
+        throw new NotImplementedException();
+    }
+}
